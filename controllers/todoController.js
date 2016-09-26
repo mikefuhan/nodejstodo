1f16e637b0ee0aaf1958194cb76803ac9062c3a2
mikefuhan <wongfuhan@gmail.com>
2016-09-26 11:15:26 +0800

    updated todoController.js
    Signed-off-by: mikefuhan <wongfuhan@gmail.com>

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -3,8 +3,9 @@ var mongoose = require('mongoose');   // mongoose for connecting to mongoDB
 
 // connect to mlab mongodb
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://test1:test1@127.0.0.1:27017/Todo');
+mongoose.connect('mongodb://test1:test1@127.0.0.1:27017/Todo'); // this line is for windows local mongodb
 // mongoose.connect('mongodb://test1:test1@ds035806.mlab.com:35806/foobartodo'); // For mLab mongodb
+
 // schema
 var todoSchema = new mongoose.Schema({
   item: String
@@ -17,14 +18,13 @@ var currentdate = new Date();
 var datetime = currentdate.getDate() + "/"+ (currentdate.getMonth()+1) + "/" + currentdate.getFullYear() + " @ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
 console.log(datetime);
 
+/* testing only, as this will add a new line of item into collection everytime this app runs
 var itemTestOnly = Todo({item:datetime}).save(function(err){
   if (err) throw err;
   console.log('item saved.');
 });   // test with one item, remove when roll-out
-
-
-
-var data = [{item: 'get milk'},{item: 'walk dog'},{item: 'kick something'}];
+*/
+// var data = [{item: 'get milk'},{item: 'walk dog'},{item: 'kick something'}];
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -32,18 +32,26 @@ module.exports = function(app)
 {
 
   app.get('/todo', function(req, res){
-      res.render('todo', {todos: data});
+      // get data from mongoDB and pass it to the view
+      Todo.find({}, function(err, data){
+        if (err) throw err;
+        res.render('todo', {todos: data});
+      });
   });
 
   app.post('/todo', urlencodedParser, function(req, res){
-      data.push(req.body); // add the new item into this array
-      res.json(data);      // send this thing back as json to the front-end
+      // get data fomr the view and add it to mongodb
+      var newTodo = Todo(req.body).save(function(err, data){
+        if (err) throw err;
+        res.json(data);
+      });
   });
 
   app.delete('/todo/:item', function(req, res){
-      data = data.filter(function(todo){
-        return todo.item.replace(/ /g, '-') !== req.params.item; // when this returns false, it will filter this item out from the array.
+      // delete the requested item from mongodb
+      Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function (err,data){
+        if (err) throw err;
+        res.json(data);
       });
-      res.json(data);
   });
 }
